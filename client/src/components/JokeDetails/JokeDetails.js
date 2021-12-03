1bc49c41f392dbf514ab1cf49a8836a6a1c5f5aa
tanseanzy <tanseanzy@gmail.com>
2021-12-02 19:29:52 -0800

    comitting changes before heroku deployment

--- client/src/components/JokeDetails/JokeDetails.js
+++ client/src/components/JokeDetails/JokeDetails.js
@@ -3,15 +3,6 @@ import "./JokeDetails.scss";
 import chuckNorrisImage from "../../assets/images/chuck-norris.png";
 
 function JokeDetails({ currentJoke }) {
-  console.log(currentJoke);
-  // const joke = props.currentJoke;
-  // console.log(currentJoke);
-
-  // function getNewJoke(e) {
-  //   const jokeid = e.target.id;
-  //   console.log(jokeid);
-  // }
-
   return (
     <div className="container">
       <img
